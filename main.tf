d8434e687cc34186d97567542709db8b13ce670b
kush Kumar <kush.kumar@ssh.com>
2022-02-27 21:15:27 +0000

    added list functionality

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "s3_bucket" {
+  count        = length(var.aws_s3_bucket_data)
   source       = "./module"
-  bucket-name  = var.aws_s3_bucket_data.bucket-name
-  bucket-count = var.aws_s3_bucket_data.bucket-count
+  bucket-name  = var.aws_s3_bucket_data.bucket-data[count.index].bucket-name
+  bucket-count = var.aws_s3_bucket_data.bucket-data[count.index].bucket-count
 }
--- variables.tf
+++ variables.tf
@@ -2,7 +2,15 @@ variable "aws_s3_bucket_data" {
   description = "Specify AWS region, Bucket name prefix and Bucket count"
   default = {
     region : "eu-west-2",
-    bucket-name : "myexample-bucket",
-    bucket-count : 1
+    bucket-data : [
+      {
+        bucket-name : "ravi-bucket",
+        bucket-count : 2
+      },
+      {
+        bucket-name : "kush-bucket",
+        bucket-count : 3
+      }
+    ]
   }
 }
