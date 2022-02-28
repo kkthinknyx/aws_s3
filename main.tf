e1122214b7fc955f2e3f3fc27d8d971a0dec33c0
kkthinknyx <97991918+kkthinknyx@users.noreply.github.com>
2022-02-28 17:18:50 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
-module "s3_bucket" {
+module "s3-bucket" {
   count        = length(var.aws_s3_bucket_data)
-  source       = "./module"
+  source       = "app.terraform.io/thinknyx/s3-bucket/aws"
+  version      = "0.1.0"
   bucket-name  = var.aws_s3_bucket_data.bucket-data[count.index].bucket-name
   bucket-count = var.aws_s3_bucket_data.bucket-data[count.index].bucket-count
 }
