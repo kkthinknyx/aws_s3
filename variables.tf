fd3896045f31d59059a4049d89ae503c5bb762db
kkthinknyx <97991918+kkthinknyx@users.noreply.github.com>
2022-02-28 17:23:58 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,16 @@
 variable "aws_s3_bucket_data" {
   description = "Specify AWS region, Bucket name prefix and Bucket count"
-  type = "any"
+  default = {
+    region : "eu-west-2",
+    bucket-data : [
+      {
+        bucket-name : "ravi-bucket",
+        bucket-count : 2
+      },
+      {
+        bucket-name : "kush-bucket",
+        bucket-count : 3
+      }
+    ]
+  }
 }
